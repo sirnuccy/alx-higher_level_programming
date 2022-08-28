e66f2ffe1eb6b616c9a1ea2d6193c9f8cf581b1a
Ahmed Sanusi <sanuz.meduz@gmail.com>
2022-08-28 09:40:30 -0700

    Commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
 
     count = len(sys.argv) -1
-    sum = 0
+    add = 0
 
     for i in range(count):
-        sum += int(sys.argv[i + 1])
+        add += int(sys.argv[i + 1])
 
-    print("{}".format(sum))
+    print("{}".format(add))

b3235220dc8c911d88eaa7845c7aa5dd884d226f
Ahmed Sanusi <sanuz.meduz@gmail.com>
2022-08-28 09:30:26 -0700

    Commit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     names = dir(hidden_4)
 
     for name in names:
-        if name[:2] != "__"
-            print("{:s}.format(name))
+        if name[:2] != "__":
+            print("{:s}".format(name))
